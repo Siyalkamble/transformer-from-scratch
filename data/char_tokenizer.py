b98e4157f81242407d9ff7db9969f77f1740e276
Siyal <siyalkamble9106@gmail.com>
2026-08-26 12:41:53 +0530

    pushing char tokenizer and download data python code

---
+++ data/char_tokenizer.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+
+with open("data/tinyshakespeare.txt", 'r') as f:
+    text = f.read()
+
+class CharTokenizer:
+
+    def __init__(self, text):
+        vocab = sorted(list(set(text)))
+        self.stoi = {ch: i for i, ch in enumerate(vocab)}
+        self.itos = {i: ch for i, ch in enumerate(vocab)}
+        self.vocab_size = len(vocab)
+       
+    def encode(self, string):
+        return [self.stoi[c] for c in string]
+
+    def decode(self, integers):
+        return ''.join(self.itos[i] for i in integers)
+
+
+tok = CharTokenizer(text)
+emb = nn.Embedding(tok.vocab_size, 32)
+sample = torch.tensor(tok.encode(text[:50]))
+out = emb(sample)
+print(out.shape)  
+
+tok = CharTokenizer(text)
+test_str = "hello world"
+assert tok.decode(tok.encode(test_str)) == test_str
+print("round-trip passed")
