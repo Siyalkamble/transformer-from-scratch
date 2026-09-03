366bdf00f80f5dc539b54911a6e4388694139b3b
Siyal <siyalkamble9106@gmail.com>
2026-09-03 23:54:44 +0530

    adding prepare.py file

--- data/char_tokenizer.py
+++ data/char_tokenizer.py
@@ -1,8 +1,3 @@
-import torch
-import torch.nn as nn
-
-with open("data/tinyshakespeare.txt", 'r') as f:
-    text = f.read()
 
 class CharTokenizer:
 
@@ -18,21 +13,3 @@ class CharTokenizer:
     def decode(self, integers):
         return ''.join(self.itos[i] for i in integers)
 
-
-tok = CharTokenizer(text)
-emb = nn.Embedding(tok.vocab_size, 32)
-sample = torch.tensor(tok.encode(text[:50]))
-out = emb(sample)
-print(tok.vocab_size) # 65
-print(out.shape)  
-
-token_ids = torch.tensor(tok.encode(text), dtype=torch.long)
-print(token_ids[:100])
-
-n = int(0.9 * len(token_ids))
-train_tok = token_ids[:n]
-val_tok = token_ids[n:]
-
-test_str = "hello world"
-assert tok.decode(tok.encode(test_str)) == test_str
-print("round-trip passed")
