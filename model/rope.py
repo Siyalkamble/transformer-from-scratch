da607b63d07cf0bfe3ff7c4f49e1d410b9b526e3
Siyal <siyalkamble9106@gmail.com>
2026-09-11 17:31:13 +0530

    staring rope

---
+++ model/rope.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+import math
+
+class RotaryPositionEncoding(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+
+    def rotate(self):
+        pass
+
+        
